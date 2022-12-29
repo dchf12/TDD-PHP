4798b9775241bd0c9508e1550fd136d44d22a7de
dchf <dchf.jp@gmail.com>
2022-12-29 21:39:59 +0900

    コンパイルエラー解消してテストを失敗させる

---
+++ money-example/app/Models/Dollar.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+final class Dollar{
+	public function __construct(public int $amount)
+	{
+	}
+
+	public function times(int $multiplier):void
+	{
+	}
+
+
+}
+
--- money-example/tests/Unit/MoneyTest.php
+++ money-example/tests/Unit/MoneyTest.php
@@ -1,18 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Models\Dollar;
 
 class MoneyTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+			
+	public function setUp():void
+	{
+		parent::setUp();
+	}
+
+	public function testMultiplication()
+	{
+		$five = new Dollar(5);
+		$five->times(2);
+		$this->assertSame(10, $five->amount);
+	}
 }
