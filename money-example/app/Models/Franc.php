d43a394dd7fe21d3fe9f0de233c1e51b21ad4b7b
dchf <dchf.jp@gmail.com>
2022-12-30 15:04:18 +0900

    Francのequals()メソッドをMoneyクラスへ移動

--- money-example/app/Models/Franc.php
+++ money-example/app/Models/Franc.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-final class Franc {
-	public function __construct(private int $amount)
+final class Franc extends Money {
+	public function __construct(protected int $amount)
 	{
 	}
 
 	public function times(int $multiplier): Franc
 	{
 		return new Franc($this->amount * $multiplier);
-	}
-
-	public function equals(Franc $franc):bool
-	{
-		return $this->amount === $franc->amount;
 	}
 }
 
